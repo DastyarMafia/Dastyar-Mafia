1a424cb0c9a73beb64767d3faa73c97b0befa4d4
kian ahmadian <dev.kian.ahmadian@gmail.com>
2023-02-12 23:38:01 +0330

    Fix back button

--- app/src/main/java/com/test/dastyarmafia/AboutUs.java
+++ app/src/main/java/com/test/dastyarmafia/AboutUs.java
@@ -3,6 +3,7 @@ package com.test.dastyarmafia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        ImageButton back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(view -> onBackPressed());
     }
 }
